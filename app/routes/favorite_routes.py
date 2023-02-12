caee5db9a8eb605528ecc08aac42089d6d4ac482
yangsu <suyang.sophia@outlook.com>
2023-02-11 23:23:57 -0800

    favorites routes finished

--- app/routes/favorite_routes.py
+++ app/routes/favorite_routes.py
@@ -3,3 +3,24 @@ from app import db
 from app.models.favorite import Favorite
 
 favorite_bp = Blueprint("favorites", __name__, url_prefix="/favorites")
+# POST create new favorit
+@favorite_bp.route("/<user_id>", methods=["POST"])
+def create_favorit(user_id):
+    request_body = request.get_json()
+    if "title" not in request_body or "image" not in request_body:
+        return jsonify({"message": "Title and image must be specified."}, 400)
+    new_favorit = Favorite(
+        title = request_body["title"],
+        image = request_body["image"],
+        user_id = user_id
+    )
+    db.session.add(new_favorit)
+    db.session.commit()
+    return make_response(jsonify({"title":new_favorit.title,"user_id":new_favorit.user_id}),201)
+@favorite_bp.route("/<fav_id>", methods=["DELETE"])
+def delete_fav(fav_id):
+    fav = Favorite.query.get(fav_id)
+    db.session.delete(fav)
+    db.session.commit()
+    return make_response(f"#{fav_id} successfully deleted", 200)
+
